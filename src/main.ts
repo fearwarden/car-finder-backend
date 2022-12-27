b9bf47df53a985a3626a714fcb91a1d6d4ddf11b
Boris Antonijev <antonijevboris@gmail.com>
2022-12-27 20:06:03 +0100

    passport installed

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = parseInt(process.env.PORT) || 3000;
+  app.setGlobalPrefix('api/v1');
   await app.listen(PORT);
 }
 bootstrap();
